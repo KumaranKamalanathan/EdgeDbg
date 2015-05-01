5a238ff41b314f76e1b71affddbdeb30dc7feebb
SkyLined <berendjanwever@gmail.com>
2015-05-01 09:48:46 +0200

    Many changes for v0.5

--- ProjectSpartanDbg.cpp
+++ ProjectSpartanDbg.cpp
@@ -33,7 +33,7 @@ HRESULT fGetProcessIdForExecutableName(_TCHAR* sExecutableName, DWORD &dwProcess
     PROCESSENTRY32 oProcessEntry32;
     oProcessEntry32.dwSize = sizeof(oProcessEntry32);
     if (!Process32First(hProcessesSnapshot, &oProcessEntry32)) {
-      _tprintf(_T("Cannot get first process from snapshot\r\n"));
+      _tprintf(_T("Cannot get first process from snapshot (error %08x)\r\n"), GetLastError());
       hResult = HRESULT_FROM_WIN32(GetLastError());
     } else do {
       HANDLE hModulesSnapshot;
@@ -42,7 +42,7 @@ HRESULT fGetProcessIdForExecutableName(_TCHAR* sExecutableName, DWORD &dwProcess
         MODULEENTRY32 oModuleEntry32;
         oModuleEntry32.dwSize = sizeof(oModuleEntry32);
         if (!Module32First(hModulesSnapshot, &oModuleEntry32)) {
-          _tprintf(_T("Cannot get first module from snapshot\r\n"));
+          _tprintf(_T("Cannot get first module from snapshot (error %08X)\r\n"), GetLastError());
           hResult = HRESULT_FROM_WIN32(GetLastError());
         } else do {
           if (_tcscmp(oModuleEntry32.szModule, sExecutableName) == 0) {
@@ -73,17 +73,17 @@ HRESULT fSuspendThreadsInProcessById(DWORD dwProcessId) {
     THREADENTRY32 oThreadEntry32;
     oThreadEntry32.dwSize = sizeof(oThreadEntry32);
     if (!Thread32First(hThreadSnapshot, &oThreadEntry32)) {
-      _tprintf(_T("Cannot get first thread from snapshot\r\n"));
+      _tprintf(_T("Cannot get first thread from snapshot (error %08X)\r\n"), GetLastError());
       hResult = HRESULT_FROM_WIN32(GetLastError());
     } else do {
       if (oThreadEntry32.th32OwnerProcessID == dwProcessId) {
         HANDLE hThread = OpenThread(THREAD_ALL_ACCESS, FALSE, oThreadEntry32.th32ThreadID);
         if (!hThread) {
-          _tprintf(_T("Cannot open thread %d of process %d"), oThreadEntry32.th32ThreadID, oThreadEntry32.th32OwnerProcessID);
+          _tprintf(_T("Cannot open thread %d of process %d (error %08X)"), oThreadEntry32.th32ThreadID, oThreadEntry32.th32OwnerProcessID, GetLastError());
           hResult = HRESULT_FROM_WIN32(GetLastError());
         } else {
           if (SuspendThread(hThread) == -1) {
-            _tprintf(_T("Cannot suspend thread %d of process %d"), oThreadEntry32.th32ThreadID, oThreadEntry32.th32OwnerProcessID);
+            _tprintf(_T("Cannot suspend thread %d of process %d (error %08X)"), oThreadEntry32.th32ThreadID, oThreadEntry32.th32OwnerProcessID, GetLastError());
             hResult = HRESULT_FROM_WIN32(GetLastError());
           }
           if (!fCloseHandleAndUpdateResult(hThread, hResult)) {
@@ -94,6 +94,45 @@ HRESULT fSuspendThreadsInProcessById(DWORD dwProcessId) {
     } while (SUCCEEDED(hResult) && Thread32Next(hThreadSnapshot, &oThreadEntry32));
     if (!fCloseHandleAndUpdateResult(hThreadSnapshot, hResult)) {
       _tprintf(_T("Cannot close threads snapshot\r\n"));
+    }
+  }
+  return hResult;
+}
+HRESULT fShowProcessIdsAndSuspendThreadsForExecutableName(_TCHAR* sExecutableName, BOOL bSuspendThreads) {
+  HANDLE hProcessesSnapshot;
+  HRESULT hResult = fGetSnapshot(TH32CS_SNAPPROCESS, 0, hProcessesSnapshot);
+  if (!SUCCEEDED(hResult)) {
+    _tprintf(_T("Cannot create processes snapshot\r\n"));
+  } else {
+    PROCESSENTRY32 oProcessEntry32;
+    oProcessEntry32.dwSize = sizeof(oProcessEntry32);
+    if (!Process32First(hProcessesSnapshot, &oProcessEntry32)) {
+      _tprintf(_T("Cannot get first process from snapshot (error %08x)\r\n"), GetLastError());
+      hResult = HRESULT_FROM_WIN32(GetLastError());
+    } else do {
+      HANDLE hModulesSnapshot;
+      HRESULT hResult = fGetSnapshot(TH32CS_SNAPMODULE, oProcessEntry32.th32ProcessID, hModulesSnapshot);
+      if (SUCCEEDED(hResult)) { // ignore errors: we do not have access to all processes on the system.
+        MODULEENTRY32 oModuleEntry32;
+        oModuleEntry32.dwSize = sizeof(oModuleEntry32);
+        if (!Module32First(hModulesSnapshot, &oModuleEntry32)) {
+          _tprintf(_T("Cannot get first module from snapshot (error %08X)\r\n"), GetLastError());
+          hResult = HRESULT_FROM_WIN32(GetLastError());
+        } else do {
+          if (_tcscmp(oModuleEntry32.szModule, sExecutableName) == 0) {
+            _tprintf(_T("%s process id = %d"), sExecutableName, oModuleEntry32.th32ProcessID);
+            if (bSuspendThreads) {
+              hResult = fSuspendThreadsInProcessById(oModuleEntry32.th32ProcessID);
+            }
+          }
+        } while (SUCCEEDED(hResult) && Module32Next(hModulesSnapshot, &oModuleEntry32));
+        if (!fCloseHandleAndUpdateResult(hModulesSnapshot, hResult)) {
+          _tprintf(_T("Cannot close modules snapshot\r\n"));
+        }
+      }
+    } while (SUCCEEDED(hResult) && Process32Next(hProcessesSnapshot, &oProcessEntry32));
+    if (!fCloseHandleAndUpdateResult(hProcessesSnapshot, hResult)) {
+      _tprintf(_T("Cannot close processes snapshot\r\n"));
     }
   }
   return hResult;
@@ -149,10 +188,10 @@ HRESULT fRunDebugger(DWORD dwSpartanProcessId, DWORD dwBrowserBrokerProcessId, U
       hResult = S_OK;
     }
     if (!fCloseHandleAndUpdateResult(oProcessInformation.hProcess, hResult)) {
-      _tprintf(_T("Cannot close debugger process %d\r\n"), oProcessInformation.dwProcessId);
+      _tprintf(_T("Cannot close debugger process %d (error %08X)\r\n"), oProcessInformation.dwProcessId, GetLastError());
     }
     if (!fCloseHandleAndUpdateResult(oProcessInformation.hThread, hResult)) {
-      _tprintf(_T("Cannot close debugger thread %d\r\n"), oProcessInformation.dwThreadId);
+      _tprintf(_T("Cannot close debugger thread %d (error %08X)\r\n"), oProcessInformation.dwThreadId, GetLastError());
     }
   }
   return hResult;
@@ -161,17 +200,33 @@ HRESULT fKillProcessById(DWORD dwProcessId) {
   HRESULT hResult;
   HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, dwProcessId);
   if (!hProcess) {
-    _tprintf(_T("Cannot open process %d\r\n"), dwProcessId);
+    _tprintf(_T("Cannot open process %d (error %08X)\r\n"), dwProcessId, GetLastError());
     hResult = HRESULT_FROM_WIN32(GetLastError());
   } else {
-    if (!TerminateProcess(hProcess, 0)) {
-      _tprintf(_T("Cannot terminate process %d\r\n"), dwProcessId);
+    if (!TerminateProcess(hProcess, 0) && WaitForSingleObject(hProcess, 0) == WAIT_TIMEOUT) {
+      _tprintf(_T("Cannot terminate process %d (error %08X)\r\n"), dwProcessId, GetLastError());
       hResult = HRESULT_FROM_WIN32(GetLastError());
+    } else {
+      hResult = S_OK;
     }
     if (!fCloseHandleAndUpdateResult(hProcess, hResult)) {
       _tprintf(_T("Cannot close process %d\r\n")), dwProcessId;
     }
   }
+  return hResult;
+}
+HRESULT fTerminateProcessesForExecutableName(_TCHAR* sExecutableName) {
+  HRESULT hResult;
+  DWORD dwProcessId;
+  do {
+    hResult = fGetProcessIdForExecutableName(sExecutableName, dwProcessId);
+    if (SUCCEEDED(hResult) && dwProcessId) {
+      hResult = fKillProcessById(dwProcessId);
+      if (SUCCEEDED(hResult)) {
+        _tprintf(_T("Terminated %s process %d\r\n"), sExecutableName, dwProcessId);
+      }
+    }
+  } while (SUCCEEDED(hResult) && dwProcessId);
   return hResult;
 }
 int _tmain(UINT uArgumentsCount, _TCHAR* asArguments[]) {
@@ -193,36 +248,48 @@ int _tmain(UINT uArgumentsCount, _TCHAR* asArguments[]) {
       if (!SUCCEEDED(hResult)) {
         _tprintf(_T("Failed to create Application Activation Manager\r\n"));
       } else {
-        DWORD dwSpartanProcessId;
-        hResult = pAAM->ActivateApplication(sSpartanAUMID, asArguments[1], AO_NONE, &dwSpartanProcessId);
-        if (!SUCCEEDED(hResult)) {
-          _tprintf(_T("Failed to launch Project Spartan\r\n"));
-        } else {
-          _tprintf(_T("Project Spartan process id = %d\r\n"), dwSpartanProcessId);
-          hResult = fSuspendThreadsInProcessById(dwSpartanProcessId);
-          if (SUCCEEDED(hResult)) {
-            DWORD dwBrowserBrokerProcessId;
-            hResult = fGetProcessIdForExecutableName(_T("browser_broker.exe"), dwBrowserBrokerProcessId);
+        hResult = fTerminateProcessesForExecutableName(_T("spartan.exe"));
+        if (SUCCEEDED(hResult)) {
+          hResult = fTerminateProcessesForExecutableName(_T("browser_broker.exe"));
+        }
+        if (SUCCEEDED(hResult)) {
+          hResult = fTerminateProcessesForExecutableName(_T("spartan_edge.exe"));
+        }
+        if (SUCCEEDED(hResult)) {
+          DWORD dwSpartanProcessId;
+          hResult = pAAM->ActivateApplication(sSpartanAUMID, asArguments[1], AO_NONE, &dwSpartanProcessId);
+          if (!SUCCEEDED(hResult)) {
+            _tprintf(_T("Failed to launch Project Spartan\r\n"));
+          } else {
+            BOOL bSuspendThreads = uArgumentsCount > 2;
+            if (bSuspendThreads) {
+              hResult = fSuspendThreadsInProcessById(dwSpartanProcessId);
+            }
             if (SUCCEEDED(hResult)) {
-              if (!dwBrowserBrokerProcessId) {
-                _tprintf(_T("Browser broker process not found!\r\n"));
-                hResult = HRESULT_FROM_WIN32(ERROR_INVALID_PARAMETER); // similar to cdb behavior
-              } else {
-                _tprintf(_T("Browser broker process id = %d\r\n"), dwBrowserBrokerProcessId);
-                hResult = fSuspendThreadsInProcessById(dwBrowserBrokerProcessId);
-                if (SUCCEEDED(hResult)) {
-                  DWORD dwSpartanEdgeProcessId;
-                  do {
-                    hResult = fGetProcessIdForExecutableName(_T("spartan_edge.exe"), dwSpartanEdgeProcessId);
-                    if (SUCCEEDED(hResult) && dwSpartanEdgeProcessId) {
-                      hResult = fKillProcessById(dwSpartanEdgeProcessId);
-                      if (SUCCEEDED(hResult)) {
-                        _tprintf(_T("Terminated spartan edge process %d\r\n"), dwSpartanEdgeProcessId);
+              DWORD dwBrowserBrokerProcessId;
+              hResult = fGetProcessIdForExecutableName(_T("browser_broker.exe"), dwBrowserBrokerProcessId);
+              if (SUCCEEDED(hResult)) {
+                if (!dwBrowserBrokerProcessId) {
+                  _tprintf(_T("Browser broker process not found!\r\n"));
+                  hResult = HRESULT_FROM_WIN32(ERROR_INVALID_PARAMETER); // similar to cdb behavior
+                } else {
+                  if (bSuspendThreads) {
+                    hResult = fSuspendThreadsInProcessById(dwBrowserBrokerProcessId);
+                  }
+                  if (SUCCEEDED(hResult)) {
+                    _tprintf(_T("spartan.exe process id = %d\r\n"), dwSpartanProcessId);
+                    _tprintf(_T("browser_broker.exe process id = %d\r\n"), dwBrowserBrokerProcessId);
+                    BOOL bRunDebugger = uArgumentsCount > 3 || (uArgumentsCount == 3 && _tcscmp(asArguments[2], _T("@")) != 0);
+                    if (bRunDebugger) {
+                      hResult = fTerminateProcessesForExecutableName(_T("spartan_edge.exe"));
+                    } else {
+                      hResult = fShowProcessIdsAndSuspendThreadsForExecutableName(_T("spartan_edge.exe"), bSuspendThreads);
+                    }
+                    if (SUCCEEDED(hResult)) {
+                      if (bRunDebugger) {
+                        hResult = fRunDebugger(dwSpartanProcessId, dwBrowserBrokerProcessId, uArgumentsCount - 2, asArguments + 2);
                       }
                     }
-                  } while (SUCCEEDED(hResult) && dwSpartanEdgeProcessId);
-                  if (SUCCEEDED(hResult) && uArgumentsCount > 2) {
-                    hResult = fRunDebugger(dwSpartanProcessId, dwBrowserBrokerProcessId, uArgumentsCount - 2, asArguments + 2);
                   }
                 }
               }
