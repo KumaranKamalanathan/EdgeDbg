c9b2ccb0999bc95c31b4d39aba192beae35741ca
SkyLined <berendjanwever@gmail.com>
2015-08-26 18:46:06 +0200

    Fix bug in process resuming

--- EdgeWinDbg.cmd
+++ EdgeWinDbg.cmd
@@ -20,4 +20,13 @@ ECHO * Deleting crash recovery data...
 DEL "%LOCALAPPDATA%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\Recovery\Active\*.*" /Q >nul
 
 ECHO * Starting Edge in Windbg...
-"%EdgeDbg%" %2 %1 -o -p @MicrosoftEdge@ -c "~*m;.attach 0n@MicrosoftEdgeCP@;g;~*m;.attach 0n@browser_broker@;g;~*m;.attach 0n@RuntimeBroker@;g;~*m;g" %3 %4 %5 %6 %7 %8 %9
+:: 1) Attach to first process through "-p" command line argument
+:: 2) Attach to remaining three processes using ".attach" & "g"
+:: We cannot resume the processes yet, as attaching requires "g", which will cause the processes to run, which may
+:: trigger an exception in one of the processes, which will then interfere with the remaining commands we are going to
+:: execute and everything will be one big mess.
+:: 3) Resume threads in current process (last attached process)
+:: 4) And resume first three processes (set current and resume)
+:: 5) Continue the processes ("g")
+:: This way all processes are resumed at the same time.
+"%EdgeDbg%" %2 %1 -o -p @MicrosoftEdge@ -c ".attach 0n@MicrosoftEdgeCP@;g;.attach 0n@browser_broker@;g;.attach 0n@RuntimeBroker@;g;~*m;|0s;~*m;|1s;~*m;|2s;~*m;g" %3 %4 %5 %6 %7 %8 %9
