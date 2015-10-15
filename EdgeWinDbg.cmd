14cfe021fe67258fd44ddd0861d09a7d861b087d
SkyLined <berendjanwever@gmail.com>
2015-10-15 15:01:21 +0200

    Update cmd scripts
    * Use binaries from bin
    * Use Kill

--- EdgeWinDbg.cmd
+++ EdgeWinDbg.cmd
@@ -15,14 +15,27 @@ IF NOT EXIST "%WinDbg%" (
 
 IF NOT "%EdgeDbg:~0,0%" == "" (
   IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
-    SET EdgeDbg=%~dp0EdgeDbg_x64.exe
+    SET EdgeDbg=%~dp0bin\EdgeDbg_x64.exe
   ) ELSE (
-    SET EdgeDbg=%~dp0EdgeDbg_x86.exe
+    SET EdgeDbg=%~dp0bin\EdgeDbg_x86.exe
   )
 )
 SET EdgeDbg=%EdgeDbg:"=%
 IF NOT EXIST "%EdgeDbg%" (
   ECHO - Cannot find EdgeDbg at "%EdgeDbg%", please set the "EdgeDbg" environment variable to the correct path.
+  EXIT /B 1
+)
+
+IF NOT "%Kill:~0,0%" == "" (
+  IF "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+    SET Kill=%~dp0src\Kill\bin\Kill_x64.exe
+  ) ELSE (
+    SET Kill=%~dp0src\Kill\bin\Kill_x86.exe
+  )
+)
+SET Kill=%Kill:"=%
+IF NOT EXIST "%Kill%" (
+  ECHO - Cannot find Kill at "%Kill%", please set the "Kill" environment variable to the correct path.
   EXIT /B 1
 )
 
@@ -34,11 +47,12 @@ IF "%~1" == "" (
   SET WinDbgArguments=%2 %3 %4 %5 %6 %7 %8 %9
 )
 
-ECHO * Terminating any running Edge processes...
-TASKKILL /F /IM MicrosoftEdge.exe 2>nul >nul
-TASKKILL /F /IM browser_broker.exe 2>nul >nul
-TASKKILL /F /IM RuntimeBroker.exe 2>nul >nul
-TASKKILL /F /IM MicrosoftEdgeCP.exe 2>nul >nul
+ECHO * Terminating all running processes associated with Edge...
+"%Kill%" MicrosoftEdge.exe browser_broker.exe RuntimeBroker.exe MicrosoftEdgeCP.exe
+IF ERRORLEVEL 1 (
+  ECHO - Cannot terminate all running processes associated with Edge.
+  EXIT /B 1
+)
 
 IF EXIST "%LOCALAPPDATA%\Packages\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\AC\MicrosoftEdge\User\Default\Recovery\Active\*.*" (
   ECHO * Deleting crash recovery data...
