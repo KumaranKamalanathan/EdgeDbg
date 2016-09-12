2e763a80924d256ec918562248d86948d909d14b
SkyLined <berendjanwever@gmail.com>
2016-09-12 17:27:22 +0200

    Kill Edge before spawning

--- Tests.py
+++ Tests.py
@@ -1,6 +1,10 @@
 from fauStartEdgeAndReturnProcessIds import fauStartEdgeAndReturnProcessIds;
 from Kill import fKillProcessesUntilTheyAreDead;
 
+print "* Killing any running instances of Edge...";
+fKillProcessesUntilTheyAreDead(asBinaryNames = [
+  "ApplicationFrameHost.exe", "browser_broker.exe", "MicrosoftEdge.exe", "MicrosoftEdgeCP.exe", "RuntimeBroker.exe",
+]);
 print "* Starting Edge and returning process ids...";
 auProcessIds = fauStartEdgeAndReturnProcessIds();
 print repr(auProcessIds);
